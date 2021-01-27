a7b95849e6a44854f4f75171d982eb6220d3e24a
areebaqamar021 <areebaqamar021@gmail.coom>
2021-01-27 16:11:18 +0500

    Updates

---
+++ precache-manifest.475fe9723290f084ebe894a858060004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44440449a105699f0ac50127c7a88a1d",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "f6642f9913d26081dc8c",
+    "url": "/chat-app/static/css/main.972c5e40.chunk.css"
+  },
+  {
+    "revision": "3c72593d877d84a43bcb",
+    "url": "/chat-app/static/js/2.5ddd7166.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/chat-app/static/js/2.5ddd7166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6642f9913d26081dc8c",
+    "url": "/chat-app/static/js/main.2161184e.chunk.js"
+  },
+  {
+    "revision": "0c61a64e5f50ef78cada",
+    "url": "/chat-app/static/js/runtime-main.620a975f.js"
+  }
+]);
