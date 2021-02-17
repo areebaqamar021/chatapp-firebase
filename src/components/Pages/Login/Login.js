80fc649708969d03afcd3dd0dea91db74b6ec89b
areebaqamar021 <areebaqamar021@gmail.com>
2021-02-17 06:35:59 +0500

    navttc project done

---
+++ src/components/Pages/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router'
+
+import PageTitle from '../../UI/PageTitle/PageTitle'
+import LoginForm from './LoginForm/LoginForm'
+
+import commonStyle from '../../../static/style/common.module.css'
+
+function Login(props) {
+    return (
+        <div className={`my-5 pt-2 container ${commonStyle.PageBody}`}>
+            {props.user ? <Redirect to="/" /> : null}
+            
+            <PageTitle>
+            <br />
+            <br />
+                Login
+            </PageTitle>
+
+            <LoginForm />
+        </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    user: state.auth.user
+})
+
+export default connect(mapStateToProps)(Login)
