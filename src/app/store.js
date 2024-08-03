1b6c45d3d442d43112de32f2fa63802a2ee4d03c
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-03 21:02:03 +0500

    Update redux store configuration and user/chat slices

---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+// src/app/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../features/userSlice";
+import chatReducer from "../features/chatSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    chat: chatReducer,
+  },
+});
---
+++ src/features/chatSlice.js
@@ -0,0 +1,18 @@
+// src/features/chatSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const chatSlice = createSlice({
+  name: "chat",
+  initialState: { messages: [], groupChats: [] },
+  reducers: {
+    setMessages: (state, action) => {
+      state.messages = action.payload;
+    },
+    setGroupChats: (state, action) => {
+      state.groupChats = action.payload;
+    },
+  },
+});
+
+export const { setMessages, setGroupChats } = chatSlice.actions;
+export default chatSlice.reducer;
