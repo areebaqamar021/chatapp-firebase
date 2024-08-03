d706193f127054071e5e7f0c4034b54f1089e863
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-03 21:27:40 +0500

    Update Tailwind CSS configuration and dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Auth from './components/Auth';
+import Chat from './components/Chat';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/reset.css';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
-function App() {
+const { Header, Content } = Layout;
+
+const App = () => {
+  const user = useSelector((state) => state.user);
+
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      <Header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Chat App</Typography>
+          </Toolbar>
+        </AppBar>
+      </Header>
+      <Content style={{ padding: '50px' }}>
+        {user ? <Chat /> : <Auth />}
+      </Content>
+    </Layout>
   );
-}
+};
 
 export default App;
