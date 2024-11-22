4d6eee407dc820cd8dee597b22eb428339985236
areebaqamar021 <areebaqamar021@gmail.com>
2024-11-22 11:47:34 +0500

    Refactor project structure and update configurations for improved setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import './App.css'
-
-function App() {
+const App = () => {
   return (
-    <>
-     <div>
-        Chat App
-    </div>
-    </>
+    <div className=''>App</div>
   )
 }
 
-export default App;
+export default App
