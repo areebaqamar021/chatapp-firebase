5e70070d57eb5059a5e4bb5893b11669fb10e4b5
areebaqamar021 <areebaqamar021@gmail.com>
2024-11-25 20:35:29 +0500

    Add ChatList and Userinfo components to List; update styles for layout

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,12 @@
+import ChatList from "./chatList/ChatList"
 import "./list.css"
+import Userinfo from "./userInfo/Userinfo"
 
 const List = () => {
   return (
     <div className='list'>
-      List
+      <Userinfo/>
+      <ChatList/>
     </div>
   )
 }
